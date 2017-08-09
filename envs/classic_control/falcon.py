6fd26d51cdf41cba05a2b4a0d384d0892e86e8ed
EmbersArc <s-niederberger@hotmail.com>
2017-08-09 09:12:32 +0200

    improvements and cleanup

--- envs/classic_control/falcon.py
+++ envs/classic_control/falcon.py
@@ -4,6 +4,7 @@ import gym
 from gym import spaces
 from gym.utils import seeding
 import numpy as np
+import random
 
 logger = logging.getLogger(__name__)
 
@@ -18,23 +19,23 @@ class FalconEnv(gym.Env):
         self.gravity = 9.81
         self.mass = 1.0
         self.momentofinertia = 1.0
-        self.length = 0.5  # actually half the rocket's length
-        self.mass_length = (self.mass * self.length)
+        self.length = 1.0  # actually half the rocket's length
         self.dt = 0.02  # seconds between state updates
-        self.groundheight = 1
-        self.thrustmultiplier = 20
+        self.groundheight = 1.0
+        self.startheight = 8.0
+        self.thrustmultiplier = 20.0  # thrust = thrustmultiplier * throttle
 
         # Angle at which to fail the episode
-        self.theta_threshold_radians = math.pi / 3
+        self.theta_threshold_radians = math.pi / 4
         self.x_threshold = 10.0
-        self.y_threshold = 10.0
-        self.phi_threshold = 1.0
+        self.y_threshold = self.startheight*2
+        self.phi_threshold = 0.3
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
         high = np.array([
             self.x_threshold * 2,
             np.finfo(np.float32).max,
-            self.y_threshold * 2,
+            self.y_threshold,
             np.finfo(np.float32).max,
             self.theta_threshold_radians * 2,
             np.finfo(np.float32).max,
@@ -46,7 +47,7 @@ class FalconEnv(gym.Env):
             -self.x_threshold * 2,
             -np.finfo(np.float32).max,
             self.groundheight,
-            np.finfo(np.float32).max,
+            -np.finfo(np.float32).max,
             -self.theta_threshold_radians * 2,
             -np.finfo(np.float32).max,
             -self.phi_threshold,
@@ -60,8 +61,6 @@ class FalconEnv(gym.Env):
         self.viewer = None
         self.state = None
 
-        self.steps_beyond_done = None
-
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
@@ -71,13 +70,13 @@ class FalconEnv(gym.Env):
         x, x_dot, y, y_dot, theta, theta_dot, phi, throttle = self.state
 
         if action == 0:
-            phi += 0.05
+            phi = -self.phi_threshold
         elif action == 1:
-            phi -= 0.05
+            phi = self.phi_threshold
         elif action == 2:
-            throttle += 0.1
-        else:
-            throttle -= 0.1
+            throttle = 1
+        elif action == 3:
+            throttle = 0.1
 
         phi = np.clip(phi, -1, 1)
         throttle = np.clip(throttle, 0.1, 1.0)
@@ -89,27 +88,17 @@ class FalconEnv(gym.Env):
         theta = theta + self.dt * theta_dot
         x_dot = x_dot + 1 / self.mass * math.sin(theta + phi) * force_mag * self.dt
         y_dot = y_dot + (1 / self.mass * math.cos(theta + phi) * force_mag - self.gravity) * self.dt
-        theta_dot = theta_dot - (1 / self.momentofinertia) * np.sin(phi) * force_mag * self.length * self.dt
+        theta_dot = theta_dot - (1 / self.momentofinertia) * np.sin(phi) * force_mag * self.length / 2 * self.dt
 
         self.state = (x, x_dot, y, y_dot, theta, theta_dot, phi, throttle)
         done = y < self.groundheight \
-               or abs(x) > self.x_threshold \
-               or abs(theta) > self.theta_threshold_radians \
-               or y > 20
-        done = bool(done)
+            or y > self.y_threshold \
+            or abs(x) > self.x_threshold \
+            or abs(theta) > self.theta_threshold_radians
 
         if not done:
             reward = 1  # TODO
-        elif self.steps_beyond_done is None:
-            self.steps_beyond_done = 0
-            reward = 1.0
         else:
-            if self.steps_beyond_done == 0:
-                logger.warning(
-                    "You are calling 'step()' even though this environment has already returned done = True.\
-                     You should always call 'reset()' once you receive 'done = True' -- any further steps are\
-                      undefined behavior.")
-            self.steps_beyond_done += 1
             reward = 0.0
 
         return np.array(self.state), reward, done, {}
@@ -117,14 +106,14 @@ class FalconEnv(gym.Env):
     def _reset(self):
         # self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(8,))
         self.state = [
-            0,      # x
+            random.randint(-3, 3),  # x
             0,
-            10,     # y
+            self.startheight + random.randint(-3, 3),  # y
             0,
             float(np.random.randint(-4, 4)) / 10,  # theta
             float(np.random.randint(-4, 4)) / 10,  # theta_dot
-            float(np.random.randint(-2, 2)) / 10,   # phi
-            float(np.random.randint(1, 4)) / 10    # throttle
+            float(np.random.randint(-2, 2)) / 10,  # phi
+            float(np.random.randint(1, 4)) / 10  # throttle
         ]
         self.steps_beyond_done = None
         return np.array(self.state)
@@ -145,7 +134,7 @@ class FalconEnv(gym.Env):
         rocket_ratio = 5
         width_rocket = 20
         length_rocket = width_rocket * rocket_ratio
-        width_engine = width_rocket * 0.9
+        width_engine = width_rocket * 0.8
         width_fire = width_engine * 0.9
 
         if self.viewer is None:
@@ -161,9 +150,9 @@ class FalconEnv(gym.Env):
             self.viewer.add_geom(rocket)
 
             # engine
-            l, r, t = -width_engine / 2, width_engine / 2, width_engine * 2
-            engine = rendering.FilledPolygon([(0, 0), (l / 3, 0), (l, -t), (r, -t), (r / 3, 0)])
-            engine.set_color(0.1, 0.1, 0.1)
+            l, r, t_engine = -width_engine / 2, width_engine / 2, width_engine
+            engine = rendering.FilledPolygon([(0, 0), (l / 3, 0), (l, -t_engine), (r, -t_engine), (r / 3, 0)])
+            engine.set_color(0.4, 0.4, 0.4)
             self.enginetrans = rendering.Transform(translation=(0, -length_rocket * 1))
             engine.add_attr(self.enginetrans)
             engine.add_attr(self.rockettrans)
@@ -173,7 +162,9 @@ class FalconEnv(gym.Env):
             l, r, t = -width_fire / 2, width_fire / 2, width_fire * 2
             fire = rendering.FilledPolygon([(l, 0), (r, 0), (0, -t)])
             fire.set_color(0.8, 0, 0)
-            self.firetrans = rendering.Transform(translation=(0, -width_engine * 2))
+            self.firescale = rendering.Transform(scale=(1, 1))
+            self.firetrans = rendering.Transform(translation=(0, -t_engine))
+            fire.add_attr(self.firescale)
             fire.add_attr(self.firetrans)
             fire.add_attr(self.enginetrans)
             fire.add_attr(self.rockettrans)
@@ -192,10 +183,8 @@ class FalconEnv(gym.Env):
         x = self.state
         xpos = x[0] * scale + screen_width / 2.0
         ypos = x[2] * scale
-        logger.warning(x[7])
         self.rockettrans.set_translation(xpos, ypos)
         self.rockettrans.set_rotation(-x[4])
         self.enginetrans.set_rotation(-x[6])
-        self.firetrans.set_scale(1, x[7] * float(np.random.randint(5, 15)) / 10)
-
+        self.firescale.set_scale(newx=1, newy=x[7])
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
